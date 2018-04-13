6d808b66148f46172b3de099f48cd0eaec4926b3
David <davidjmoed@gmail.com>
2018-04-13 12:06:08 -0400

    Adding Firebase/pod functionality

---
+++ gif-game/Pods/Target Support Files/Pods-gif-game/Pods-gif-game-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_gif_game : NSObject
+@end
+@implementation PodsDummy_Pods_gif_game
+@end
---
+++ gif-game/Pods/Target Support Files/Pods-gif-gameTests/Pods-gif-gameTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_gif_gameTests : NSObject
+@end
+@implementation PodsDummy_Pods_gif_gameTests
+@end
